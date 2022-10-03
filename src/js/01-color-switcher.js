9f7b0333b55c716beda92a7581b4be6b91814275
Andrii Zorba <andrej.zorba@gmail.com>
2022-10-03 22:54:31 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  intervalID: null,
+};
 
+refs.buttonStart.addEventListener('click', onColorHandler);
+refs.buttonStop.addEventListener('click', stopColorChange);
+
+function stopColorChange() {
+  clearInterval(refs.intervalID);
+  refs.buttonStop.setAttribute('disabled', 'disabled');
+  refs.buttonStart.removeAttribute('disabled');
+}
+
+function onColorHandler() {
+  refs.intervalID = setInterval(handleColorOnBody, 1000);
+  refs.buttonStart.setAttribute('disabled', 'disabled');
+
+  const isDisabled = refs.buttonStop.hasAttribute('disabled');
+  if (isDisabled) {
+    refs.buttonStop.removeAttribute('disabled');
+  }
+}
+
+function handleColorOnBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
